eca4324975e6f8b672c93f7d5276f3f9f879dbd9
Ramesh176 <rameshsw17@gmail.com>
2024-02-03 14:26:18 +0530

    Updated index.html

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    // generate .vite/manifest.json in outDir
+    manifest: true,
+    rollupOptions: {
+      // overwrite default .html entry
+      input: "/path/to/main.js",
+    },
+  },
+});
