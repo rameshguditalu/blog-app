1628f87de9a6472b81fbab63ae1bb7f13f37b36e
Ramesh176 <rameshsw17@gmail.com>
2024-02-04 14:26:55 +0530

    Updated vite config file

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,7 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://blogger-net.netlify.app/",
-  assetsInclude: "assets",
   plugins: [react()],
 });
